ddb1114b4c7dbac63db8fd675a16e04acaa5e4e0
cpt arduous <mohitneg377@gmail.com>
2022-09-27 01:39:51 +0530

    Added deleteHead

---
+++ cplusplus/cpp-dsa/deleteAtHead.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    //constructor
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+
+};
+int main()
+{
+
+    return 0;
+}
--- cplusplus/cpp-dsa/insertAtMiddle.cpp
+++ cplusplus/cpp-dsa/insertAtMiddle.cpp
@@ -1,98 +1,105 @@
-#include<iostream>
-using namespace std;
+    #include<iostream>
+    using namespace std;
 
-//initializing a node 
+    //initializing a node 
 
-class node{
-    public:
-    int data;
-    node* next;
+    class node{
+        public:
+        int data;
+        node* next;
 
-    //constructor for creating node
-    node(int d)
+        //constructor for creating node
+        node(int d)
+        {
+            data = d;
+            next = NULL;
+        }
+    };
+    void insertAtHead(node*& head,int data)
     {
-        data = d;
-        next = NULL;
+        if(head == NULL)
+        {
+            head = new node(data);
+            return;
+        }
+        node* temp = new node(data);
+        temp -> next = head;
+        head = temp;
     }
-};
-void insertAtHead(node*& head,int data)
-{
-    if(head == NULL)
+    void insertAtTail(node*& head,int data)
     {
-        head = new node(data);
+        if(head == NULL)
+        {
+            head = new node(data);
+            return;
+        }
+        node* tail = head;
+        while(tail -> next != NULL)
+        {
+            tail = tail -> next;
+        }
+        tail -> next = new node(data);
         return;
     }
-    node* temp = new node(data);
-    temp -> next = head;
-    head = temp;
-}
-void insertAtTail(node*& head,int data)
-{
-    if(head == NULL)
-    {
-        head = new node(data);
-        return;
+    int length(node* head){
+        int cnt{};
+        while(head != NULL)
+        {
+            head = head -> next;
+            cnt++;
+        }
+        return cnt;
     }
-    node* tail = head;
-    while(tail -> next != NULL)
+    void insertAtMiddle(node*& head,int data,int pos)
     {
-        tail = tail -> next;
+        if(head == NULL && pos <= 0)
+        {
+            head = new node(data);
+            return;
+        }
+        if(length(head) < pos)
+        {
+            insertAtTail(head,data);
+        }
+        int jmp{};
+        node* prev = head;
+        while(jmp < pos-2)
+        {
+            prev = prev -> next;
+            jmp++;
+        }
+        node* temp = new node(data);
+        temp -> next = prev -> next;
+        prev -> next = temp;
+        
     }
-    tail -> next = new node(data);
-    return;
-}
-int length(node* head){
-    int cnt{};
-    while(head != NULL)
+    void printNodes(node* head)
     {
+        while(head != NULL)
+        {
+            cout<<head -> data<<" --> ";
+            head = head -> next;
+        }
+        cout<<'\n';
+    }
+    void deleteHead(node* head)
+    {
+        if(head == NULL) 
+            return;
+        node* temp = head;
         head = head -> next;
-        cnt++;
-    }
-    return cnt;
-}
-void insertAtMiddle(node*& head,int data,int pos)
-{
-    if(head == NULL && pos <= 0)
-    {
-        head = new node(data);
+        delete temp;
         return;
     }
-    if(length(head) < pos)
+    int main()
     {
-        insertAtTail(head,data);
+        node* head = NULL;  //creating a new node
+        insertAtHead(head,3);
+        insertAtHead(head,2);
+        insertAtHead(head,1);
+        printNodes(head);
+        deleteHead(head);
+        printNodes(head);
+        
+        return 0;
     }
-    int jmp{};
-    node* prev = head;
-    while(jmp < pos-2)
-    {
-        prev = prev -> next;
-        jmp++;
-    }
-    node* temp = new node(data);
-    temp -> next = prev -> next;
-    prev -> next = temp;
-    
-}
-void printNodes(node* head)
-{
-    while(head != NULL)
-    {
-        cout<<head -> data<<" --> ";
-        head = head -> next;
-    }
-    cout<<'\n';
-}
-int main()
-{
-    node* head = NULL;  //creating a new node
-    // insertAtHead(head,3);
-    // insertAtHead(head,2);
-    // insertAtHead(head,1);
-    // printNodes(head);
-    insertAtTail(head,3);
-    insertAtTail(head,2);
-    insertAtTail(head,1);
-    insertAtMiddle(head,5,2);
-    printNodes(head);
-    return 0;
-}

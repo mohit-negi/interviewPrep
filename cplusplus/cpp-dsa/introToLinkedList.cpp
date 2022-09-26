1b246eaa6d1fc815e21daa6d8d8d2c5b374d9228
cpt arduous <mohitneg377@gmail.com>
2022-09-26 17:49:14 +0530

    Added insertAtHead

---
+++ cplusplus/cpp-dsa/introToLinkedList.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+//initializing a node 
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    //constructor for creating node
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+void insertAtHead(node*&head,int data)
+{
+    if(head == NULL)
+    {
+        head = new node(data);
+        return;
+    }
+    node* temp = new node(data);
+    temp -> next = head;
+    head = temp;
+}
+void printNodes(node* head)
+{
+    while(head == NULL)
+    {
+        cout<<head -> data<<" --> ";
+        head = head -> next;
+    }
+    cout<<'\n';
+}
+int main()
+{
+    node* head = NULL;  //creating a new node
+    insertAtHead(head,3);
+    insertAtHead(head,2);
+    insertAtHead(head,1);
+    printNodes(head);
+
+    return 0;
+}
